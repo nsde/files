ebfc4048f2eb3f1f2ad36dad393eee18184b4a03
Felix <67185896+nsde@users.noreply.github.com>
2020-09-26 17:26:51 +0200

    Update and rename texturepacks.txt to texturepacks.py

---
+++ texturepacks.py
@@ -0,0 +1,6 @@
+[
+["Faithful", "1.16.3", "32", "vanilla", "", "", "https://github.com/seirin-blu/Faithful/raw/releases/Faithful-1.16.3.zip"],
+["Faithful", "1.14", "32", "vanilla", "", "", "https://raw.githubusercontent.com/FaithfulTeam/Faithful/releases/1.14.zip"],
+["Faithful", "1.12", "32", "vanilla", "", "", "https://github.com/FaithfulTeam/Faithful/raw/releases/1.12.zip"],
+["Faithful", "1.8", "32", "vanilla", "", "", "https://static.planetminecraft.com/files/resource_media/texture/faithful-1-8-for-pvp.zip"],
+]
